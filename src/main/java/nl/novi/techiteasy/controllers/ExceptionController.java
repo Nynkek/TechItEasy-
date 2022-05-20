90b2ec07fa6db97c3cffffb6a62be136a2333404
RJHARENBERG <reinardharenberg@gmail.com>
2022-05-20 12:31:00 +0200

    SecurityConfig gemaakt in huiswerk klas

--- src/main/java/nl/novi/techiteasy/controllers/ExceptionController.java
+++ src/main/java/nl/novi/techiteasy/controllers/ExceptionController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
-
-
-
     @ExceptionHandler(value = RecordNotFoundException.class)
 
     public ResponseEntity<Object> exception(RecordNotFoundException exception){
